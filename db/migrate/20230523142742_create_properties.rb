7034df5efffc1675456998de781ba7c4b45f27b2
Doheera-kosi <kupourevans99@gmail.com>
2023-05-23 07:28:34 -0700

    Add properties.rb

---
+++ db/migrate/20230523142742_create_properties.rb
@@ -0,0 +1,14 @@
+class CreateProperties < ActiveRecord::Migration[7.0]
+  def change
+    create_table :properties do |t|
+      t.string :name
+      t.string :address
+      t.integer :price
+      t.integer :rooms
+      t.integer :bedrooms
+      t.integer :bathrooms
+
+      t.timestamps
+    end
+  end
+end
